ca6bd05d3ccb8d6431cfc9c366f412f6dac36959
Desmond Hui <valueportal@gmail.com>
2025-11-27 11:51:41 -0700

    added a bit of documention to splash_art.h

--- splash_art.h
+++ splash_art.h
@@ -1,8 +1,32 @@
-//Group 2 -Assignment 2
+/********************************************************
+ * @file player.h                                       *
+ * @Brief public interface for displayng art in the game*
+ * @author CMPT201 Group 2                              *
+ * @date Nov. 27, 2025                                  *
+ ********************************************************/
+
+
+
 #ifndef SPLASH_ART_H
 #define SPLASH_ART_H
+
+/**
+* @brief Displays the art for the start screen
+*/
 void start_art();
+
+/**
+* @brief Displays the art for the end screen
+*/
 void end_art();
+
+/**
+* @brief Displays the art for the pause screen
+*/
 void pause_art();
+
+/**
+* @brief Displays the art for the quit screen
+*/
 void quit_art();
 #endif
