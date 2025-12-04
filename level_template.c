0a3b38ce1b5a1ac8f5efd0250dc3b10d735d4b4b
Desmond Hui <valueportal@gmail.com>
2025-12-04 16:14:48 -0700

    Part1 of fixing up some errors

--- level_template.c
+++ level_template.c
@@ -2,66 +2,69 @@
 #include <stdio.h>
 #include <curses.h>
 #include <time.h>
+#include "player.h"
+#include "splash_art.h"
 /*This is just a template of what a level*.c could look like.   Needs to be expanded more
 This is where you would add the maze layout and the objects.  Basically think about how the level would look like and translate it into here
 */
-void level_template(){
+int level_template(){
+	int input;
         char h = '|';
 	//represents the object the player needs to collect.  Replace 0 with the value for the level
         int key_count =0;
 	//Tracks the location of each key.  Second array is the coord as (x,y)
 	int key_location[key_count][2];
 	//NPC not a struct since its fairly similar to Player struct without much differences
-	Player NPC;
-	NPC.createPlayer(10,15);
+	Player NPC,p1;
+	createPlayer(NPC,'#',10,15);
 	drawPlayer(NPC);
 	//The npcs dialogue when you interact with him.  Replace example text with your own text
 	char npc_dialogue[] ="Hello Player";
-	time_t npc_timer1 = time();
-	time_t npc_timer2 = time();
+	time_t npc_timer1 = time(NULL);
+	time_t npc_timer2 = time(NULL);
+        createPlayer(p1,'@',0,10);
+        drawPlayer(p1);
         //Adds actual walls since border is visual only
 	//Put your level layout here
         mvprintw(25,30,"%c",h);
         mvprintw(26,30,"%c",h);
         mvprintw(27,30,"%c",h);
-        p1.createPlayer(0,10);
-        drawPlayer(p1);
         //Handles input / gamestate
         while(1){
 		//Keep track of where = is in the maze.  Replace 0,0 with the coords of =
-		if key_count = 0{
+		if (key_count == 0){
 			mvaddch('0','0',' ');
 		}
 		/* if timer1 is 3 seconds, NPC move right, 
 		if timer2 is 6 seconds NPC move left reset time		*/
-		if(time - npc_timer1 >=3){
-			movePlayerRight(NPC);
-			npc_timer1 = time();
+		if((time(NULL) - npc_timer1) >=3){
+			moveRight(NPC);
+			npc_timer1 = time(NULL);
 		}
-		if (time - npc_timer2 >=6){
-			movePlayerLeft(NPC);
-			npc_timer2 = time();
+		if ((time(NULL) - npc_timer2) >=6){
+			moveLeft(NPC);
+			npc_timer2 = time(NULL);
 		}
 		//Might see if switch case is possible here
                 if (input == KEY_RIGHT){
                         if (mvinch(p1.y,p1.x+1) == ' '){
-                        p = movePlayerRight(p1);
+                        p1 = moveRight(p1);
 			sleep(5);
                         }
 		} else if(input == KEY_LEFT){
                         if (mvinch(p1.y,p1.x-1) == ' '){
-                        p = movePlayerLeft(p1);
+                        p1 = moveLeft(p1);
 			sleep(5);
                         } 
                 } else if(input == KEY_UP){
                         if (mvinch(p1.y-1,p1.x) == ' '){
-                        p = movePlayerUp(p1);
+                        p1 = moveUp(p1);
 			sleep(5);
                         } 
 
                 } else if(input == KEY_DOWN){
                         if (mvinch(p1.y+1,p1.x) == ' '){
-                        p = movePlayerDown(p1);
+                        p1 = moveDown(p1);
 			sleep(5);
                         } 
                 } else if (input == 'q'){
---
+++ level_template.h
@@ -0,0 +1,5 @@
+#ifndef LEVEL_TEST_H
+#define LEVEL_TEST_H
+int level_test();
+void refresh_level();
+#endif
